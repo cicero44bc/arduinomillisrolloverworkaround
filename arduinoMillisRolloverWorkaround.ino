4d9d7d019d52247285e9b71635ad5538597a6403
cicero44bc <cicero44bc@users.noreply.github.com>
2016-01-03 22:26:30 +0000

    Update arduinoMillisRolloverWorkaround.ino

--- arduinoMillisRolloverWorkaround.ino
+++ arduinoMillisRolloverWorkaround.ino
@@ -1,3 +1,13 @@
+/*
+
+Arduino millis() rollover workaround example.
+
+Calculates the duration in millis between two millis values and returns the correct value even if millis() rolls over.
+
+This is useful when creating tasks that need to run for a durations that cannot be affected by millis() rolling over.
+
+*/
+
 void setup() {
   Serial.begin(9600);  
 }
